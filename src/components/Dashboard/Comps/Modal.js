1e6f35ed590add8ea7b12a33e1fc634a57a646c7
DevDre <bighalo77@gmail.com>
2025-07-16 21:49:30 +0100

    final commit

--- src/components/Dashboard/Comps/Modal.js
+++ src/components/Dashboard/Comps/Modal.js
@@ -1,17 +1,13 @@
 import { X } from "phosphor-react";
 import React from "react";
 import ModalForm from "./ModalForm";
-import { useNavigate } from "react-router-dom";
 
 const Modal = ({ showModal, handleModal }) => {
-  const navigate = useNavigate();
   return (
     <>
       <section
         id="modal"
-        className={`w-full h-screen flex justify-center items-center fixed bg-gray-900/50 ${
-          showModal ? "top-0" : "-top-[1000px]"
-        } top-0 left-0 transition-all duration-500 ease-in`}
+        className={`w-full h-screen flex justify-center items-center fixed bg-gray-900/50 top-0 left-0 transition-all duration-500 ease-in ${showModal ? "" : "hidden"}`}
       >
         <main className="lg:w-1/2 md:w-3/4 w-11/12  h-auto bg-gray-100 rounded-lg py-6 px-6">
           <div className="flex justify-end items-center text-gray-900">
